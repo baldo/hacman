4cc22f2036a2ee504afa53c856fb91e2cde8c09d
Andreas Baldeau <andreas@baldeau.net>
2011-04-24 22:33:15 +0200

    Basic -Q and -Qs.

--- src/Hacman.hs
+++ src/Hacman.hs
@@ -3,5 +3,58 @@ module Main
     )
 where
 
+import Control.Monad.Trans
+import Distribution.ArchLinux.ALPM
+
 main :: IO ()
-main = return ()
+main = do
+    r <- alpm hacmanMain
+    case r of
+        Left e ->
+            print e
+
+        Right () ->
+            return ()
+
+hacmanMain :: ALPM ()
+hacmanMain = querySearch
+
+query :: ALPM ()
+query = do
+    optionSetDatabasePath "/var/lib/pacman/"
+    db <- optionGetLocalDatabase 
+    packages <- databaseGetPackageCache db
+    mapM_ displayPackageSimple packages 
+
+querySearch :: ALPM ()
+querySearch = do
+    optionSetDatabasePath "/var/lib/pacman/"
+    db <- optionGetLocalDatabase 
+    packages <- databaseGetPackageCache db
+    mapM_ displayPackage packages
+
+displayPackageSimple :: Package -> ALPM ()
+displayPackageSimple package = do
+    name    <- packageGetName package
+    version <- packageGetVersion package
+
+    liftIO $ putStrLn $ name ++ ' ' : version
+
+displayPackage :: Package -> ALPM ()
+displayPackage package = do
+    name        <- packageGetName package
+    version     <- packageGetVersion package
+    description <- packageGetDescription package
+    groups      <- packageGetGroups package
+
+    liftIO $ do
+        putStr $ "local/" ++ name ++ ' ' : version
+        if not $ null groups
+            then do
+                putStr " ("
+                putStr $ foldl1 (\l r -> l ++ ' ' : r) groups
+                putStrLn ")"
+            else
+                putStrLn ""
+        putStrLn $ "    " ++ description
+
